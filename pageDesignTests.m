8fde16e592fdb27c342b6d50063ca4d466dcab68
Raj-LPU <er.rajnarayan@gmail.com>
2013-07-09 16:37:44 -0700

    wdfsvd
    sdfdsfg

---
+++ pageDesignTests.m
@@ -0,0 +1,32 @@
+//
+//  pageDesignTests.m
+//  pageDesignTests
+//
+//  Created by admin on 02/07/13.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import "pageDesignTests.h"
+
+@implementation pageDesignTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in pageDesignTests");
+}
+
+@end
