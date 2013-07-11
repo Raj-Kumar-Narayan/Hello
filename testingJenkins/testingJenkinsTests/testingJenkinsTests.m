ffa6d42b7e67eef3c9e0060759b0a97c2fa007c2
Raj-LPU <er.rajnarayan@gmail.com>
2013-07-11 10:41:26 -0700

    TestJenkin
    Testing of Jenkins

---
+++ testingJenkins/testingJenkinsTests/testingJenkinsTests.m
@@ -0,0 +1,32 @@
+//
+//  testingJenkinsTests.m
+//  testingJenkinsTests
+//
+//  Created by admin on 11/07/13.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import "testingJenkinsTests.h"
+
+@implementation testingJenkinsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in testingJenkinsTests");
+}
+
+@end
